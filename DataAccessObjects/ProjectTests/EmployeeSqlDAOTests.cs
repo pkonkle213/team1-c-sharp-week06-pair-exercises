9b6ef239be0ac7ffee60a92eb19214cf86b521b9
Phillip Konkle <pkonkle213@gmail.com>
2021-10-19 17:12:40 -0400

    Day 2 Part 2 Electric boogaloo

--- DataAccessObjects/ProjectTests/EmployeeSqlDAOTests.cs
+++ DataAccessObjects/ProjectTests/EmployeeSqlDAOTests.cs
@@ -7,7 +7,51 @@ using System.Text;
 
 namespace ProjectTests
 {
-    class EmployeeSqlDAOTests
+    [TestClass]
+    public class EmployeeSqlDAOTests : ProjectTestsBase
     {
+        [TestMethod]
+        public void GetAllEmployeesGetsAllEmployees()
+        {
+            //Arrange
+            EmployeeSqlDAO dao = new EmployeeSqlDAO(this.ConnectionString);
+
+            //Act
+            ICollection<Employee> emp = dao.GetAllEmployees();
+
+            //Assert
+            Assert.IsNotNull(emp);
+            Assert.AreEqual(2, GetRowCount("employee"));
+        }
+
+        [TestMethod]
+        public void GetAllEmployeesWithANameReturnsAllEmployeesWithTheName()
+        {
+            //Arrange
+            EmployeeSqlDAO dao = new EmployeeSqlDAO(this.ConnectionString);
+            string first = "Phillip";
+            string last = "Konkle";
+
+            //Act
+            ICollection<Employee> results = dao.Search(first, last);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetEmployeesWithoutProjectsShouldReturnUnassignedPeople()
+        {
+            //Arrange
+            EmployeeSqlDAO dao = new EmployeeSqlDAO(this.ConnectionString);
+
+            //Act
+            ICollection<Employee> emp = dao.GetEmployeesWithoutProjects();
+
+            //Assert
+            Assert.IsNotNull(emp);
+            Assert.IsTrue(emp.Count > 0);
+        }
     }
 }
